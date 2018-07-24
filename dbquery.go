4b23005a1182c20116b0f1ab6e60f30262f102bd
zxnhl702 <gaulle@126.com>
2018-07-24 08:42:39 +0800

    add GetMultyRowsDataWithWhere

--- dbquery.go
+++ dbquery.go
@@ -92,6 +92,81 @@ func GetMultyRowsData(sqls string, db *sql.DB) ([]*map[string]interface{}, error
     return list, nil
 }
 
+// 获取多行数据 带where条件
+func GetMultyRowsDataWithWhere(sqlCmd string, valstr string, db *sql.DB) ([]*map[string]interface{}, error) {
+    // sql where 条件
+    vals := make([]interface{}, 20)
+    json.Unmarshal([]byte(valstr), &vals)
+
+    // 返回数据
+    list := make([]*map[string]interface{}, 0)
+    // 执行sql
+    rows, err := db.Query(sqlCmd, vals...)
+    if nil != err {
+        log.Println(err)
+        log.Println("无法执行sql")
+        return list, err
+    }
+    defer rows.Close()
+
+    // 每一列的列名
+    columns, err := rows.Columns()
+    if nil != err {
+        log.Println(err)
+        log.Println("无法获取列信息")
+        return list, err
+    }
+    // 字段数量
+    size := len(columns)
+
+    for rows.Next() {
+        // 行数据的指针 用来调用scan
+        pts := make([]interface{}, size)
+        // 行数据 用来存数据
+        container := make([]interface{}, size)
+        for i := range pts {
+            pts[i] = &container[i]
+        }
+        // 扫描行数据
+        rows.Scan(pts...)
+
+        // 行数据 map形式存储
+        rowData := make(map[string]interface{})
+        // 循环取出来的每一列数据 根据不同的数据类型进行转换 然后存入map中
+        for i, v := range container {
+            log.Println(i, reflect.TypeOf(v).Name(), reflect.TypeOf(v).Kind().String())
+            // 处理列名 确定map中的key
+            col := getColumnName(i, columns[i])
+            // 根据每个字段数据类型的不同 转换成string
+            switch vv := v.(type) {
+            case int64:
+                // 整型 直接放到map中
+                log.Println("int64",  columns[i], col, vv)
+                rowData[col] = vv
+            case float64:
+                // 浮点型 直接放到map中
+                log.Println("float64",  columns[i], col, vv)
+                rowData[col] = vv
+            case bool:
+                // 布尔型 直接放到map中
+                log.Println("float64",  columns[i], col, vv)
+                rowData[col] = vv
+            case time.Time:
+                // 时间类型 转换成string 放到map中
+                log.Println("time", columns[i], col, vv.Format("2006-01-02 15:04:05"))
+                rowData[col] = vv.Format("2006-01-02 15:04:05")
+            default:
+                // 默认 先断言[]byte然后转换成string 放倒map中
+                vvv, _ :=  v.([]byte)
+                log.Println("default",  columns[i], col, string(vvv))
+                rowData[col] = string(vvv)
+            }
+        }
+        list = append(list, &rowData)
+    }
+    return list, nil
+}
+
 // 执行sql insert update delete
 func ExecSql(sqls string, db *sql.DB) (int64, error) {
     var rowid int64
