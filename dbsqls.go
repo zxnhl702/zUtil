04095126fe5d01287d40dbe62afd316fc20d4597
ZXN <gaulle@126.com>
2018-12-19 10:57:58 +0800

    增加一个使用golang template库动态生成sql的函数 但是还没测试 请慎重使用

--- dbsqls.go
+++ dbsqls.go
@@ -4,6 +4,8 @@ package zUtil
 import (
     "log"
     "regexp"
+    "strings"
+    "text/template"
 
     "github.com/flosch/pongo2"
 )
@@ -38,6 +40,31 @@ func GetDynamicSQL(p *pongo2.Context, sqltpl ...string) ([]string, error) {
         }
         // 去掉多余空格和换行 并写入数组中
         sqls = append(sqls, exp1.ReplaceAllString(s, " "))
+    }
+    return sqls, nil
+}
+
+// GetDynamicSQLWithGoTemplate 使用golang text/template模板库生成动态sql并去掉或与空格和换行 TODO Untest
+func (q QueryParam)GetDynamicSQLWithGoTemplate(sqltpl ...string) ([]string, error) {
+    var sqls []string
+    // 多余空格和换行的正则表达式
+    exp1 := regexp.MustCompile(`\s{2,}|\n`)
+    for _, v := range sqltpl {
+        tmpstr := &strings.Builder{}
+        // 初始化模板
+        t, err := template.New("tmp").Parse(v)
+        if nil != err {
+            log.Println(err)
+            return sqls, err
+        }
+        // 参数写入模板
+        err = t.Execute(tmpstr, q)
+        if nil != err {
+            log.Println(err)
+            return sqls, err
+        }
+        // 去掉多余空格和换行 并写入数组中
+        sqls = append(sqls, exp1.ReplaceAllString(tmpstr.String(), " "))
     }
     return sqls, nil
 }
