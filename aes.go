37d53c3bfb9b317fb2179da467ec8e6b82c8e2ae
ZXN <gaulle@126.com>
2020-09-25 10:55:27 +0800

    AES加密 仅限CBC模式

---
+++ aes.go
@@ -0,0 +1,122 @@
+// Package zUtil 数据加密解密的公共函数
+// AES算法加密
+package zUtil
+
+import (
+    "bytes"
+    "crypto/aes"
+    "crypto/cipher"
+    "encoding/base64"
+    "encoding/hex"
+    "log"
+)
+
+// AES加密
+type AES struct {
+    Key             []byte  // 密钥
+}
+
+// 新建 AES加密对象
+func NewAES(key string) *AES {
+    return &AES{Key : []byte(key)}
+}
+
+// 填充数据
+func padding(src []byte, blockSize int) []byte {
+    padNum := blockSize - len(src) % blockSize
+    pad := bytes.Repeat([]byte{byte(padNum)}, padNum)
+    return append(src, pad...)
+}
+
+// 去掉填充数据
+func unpadding(src []byte) []byte {
+    n := len(src)
+    unPadNum := int(src[n-1])
+    return src[:n-unPadNum]
+}
+
+// AES加密 CBC模式
+func (r *AES)AESEncryptCBC(src []byte) ([]byte, error) {
+    block, err := aes.NewCipher(r.Key)
+    if err != nil {
+        return nil, err
+    }
+    src = padding(src, block.BlockSize())
+    blockMode := cipher.NewCBCEncrypter(block, r.Key)
+    blockMode.CryptBlocks(src, src)
+    return src, nil
+}
+
+// AES解密 CBC模式
+func (r *AES)AESDecryptCBC(src []byte) ([]byte, error) {
+    block, err := aes.NewCipher(r.Key)
+    if err != nil {
+        return nil, err
+    }
+    blockMode := cipher.NewCBCDecrypter(block, r.Key)
+    blockMode.CryptBlocks(src, src)
+    src = unpadding(src)
+    return src, nil
+}
+
+// AES加密并且base64编码 CBC模式
+func (r *AES)AESEncryptCBCWithBase64(src []byte) (string, error) {
+    var base64Data string
+    // 先AES加密
+    aesData, err := r.AESEncryptCBC(src)
+    if nil != err {
+        log.Println(err)
+        return base64Data, err
+    }
+    // 再base64编码
+    base64Data = base64.StdEncoding.EncodeToString(aesData)
+    return base64Data, nil
+}
+
+// AES解密并且base64解码 CBC模式
+func (r *AES)AESDecryptCBCWithBase64(src []byte) (string, error) {
+    // 先base64解码
+    base64Data, err := base64.StdEncoding.DecodeString(string(src))
+    if nil != err {
+        log.Println(err)
+        return string(base64Data), err
+    }
+    // 再AES解密
+    aesData, err := r.AESDecryptCBC(base64Data)
+    if nil != err {
+        log.Println(err)
+        return string(aesData), err
+    }
+    return string(aesData), nil
+}
+
+// AES加密并且返回16进制字符串 CBC模式
+func (r *AES)AESEncryptCBCWithHex(src []byte) (string, error) {
+    var hexData string
+    // 先AES加密
+    aesData, err := r.AESEncryptCBC(src)
+    if nil != err {
+        log.Println(err)
+        return hexData, err
+    }
+    // 再base64编码
+    hexData = hex.EncodeToString(aesData)
+    return hexData, nil
+}
+
+// AES解密 16进制字符串 CBC模式
+func (r *AES)AESDecryptCBCWithHex(src []byte) (string, error) {
+    // 先16进制字符串转回字节数组
+    hexData, err := hex.DecodeString(string(src))
+    if nil != err {
+        log.Println(err)
+        return string(hexData), err
+    }
+    // 再AES解密
+    aesData, err := r.AESDecryptCBC(hexData)
+    if nil != err {
+        log.Println(err)
+        return string(aesData), err
+    }
+    return string(aesData), nil
+}
---
+++ aes_test.go
@@ -0,0 +1,44 @@
+// Package zUtil 数据加密解密的公共函数
+// AES的测试文件
+package zUtil
+
+import (
+    "log"
+    "testing"
+)
+
+// 测试AES加密 cbc模式 base64编码
+func TestAESEncryptCBCBase64(t *testing.T) {
+    a := NewAES("zsdy2020zsgd2020")
+    msg := "Hello World 123123 !@#"
+    encryptedMsg, _ := a.AESEncryptCBCWithBase64([]byte(msg))
+    log.Println(encryptedMsg)
+    t.Log(encryptedMsg)
+}
+
+// 测试AES解密 cbc模式 base64编码
+func TestAESDecryptCBCBase64(t *testing.T) {
+    a := NewAES("zsdy2020zsgd2020")
+    data := "H1WCYadzoggE8xRtYxjG81eaSrnGeinZOnhmFCDc0go="
+    decrpytedMsg, _ := a.AESDecryptCBCWithBase64([]byte(data))
+    log.Println(decrpytedMsg)
+    t.Log(decrpytedMsg)
+}
+
+// 测试AES加密 cbc模式 16进制字符串
+func TestAESEncryptCBCHex(t *testing.T) {
+    a := NewAES("zsdy2020zsgd2020")
+    msg := "Hello World 123123 !@#"
+    encryptedMsg, _ := a.AESEncryptCBCWithHex([]byte(msg))
+    log.Println(encryptedMsg)
+    t.Log(encryptedMsg)
+}
+
+// 测试AES解密 cbc模式 16进制字符串
+func TestAESDecryptCBCHex(t *testing.T) {
+    a := NewAES("zsdy2020zsgd2020")
+    data := "1f558261a773a20804f3146d6318c6f3579a4ab9c67a29d93a78661420dcd20a"
+    decrpytedMsg, _ := a.AESDecryptCBCWithHex([]byte(data))
+    log.Println(decrpytedMsg)
+    t.Log(decrpytedMsg)
+}
